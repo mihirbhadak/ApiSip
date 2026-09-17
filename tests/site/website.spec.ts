3b07d572d67e11adf49d706186db7809b2c1a999
Mihir Bhadak <mihirbhadak@gmail.com>
2026-09-17 16:11:07 +0530

    Update website downloads to ApiSip 0.1.5

--- tests/site/website.spec.ts
+++ tests/site/website.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 import { mkdir } from 'node:fs/promises';
 
-const release = 'https://github.com/mihirbhadak/ApiSip/releases/download/v0.1.4/apisip-0.1.4.zip';
+const release = 'https://github.com/mihirbhadak/ApiSip/releases/download/v0.1.5/apisip-0.1.5.zip';
 const coffee = 'https://buymeacoffee.com/mihir_bhadak/apisip';
 
 test('loads indexable content, local screenshots and working internal links without page errors', async ({
